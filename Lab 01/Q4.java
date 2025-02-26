3cd8ac7939adb7266324258e51531e053a9611ba
Blazie <number1tingyunsimp@gmail.com>
2025-02-26 22:37:21 +0800

    Add files via upload

---
+++ Lab 01/Q4.java
@@ -0,0 +1,55 @@
+package lab01;
+
+public class Q4 {
+    public static void main(String[] args) {
+        /*
+         * This program's function is to display data in a table into a chart
+         * (in this case, a bar chart) onto the terminal using Java
+         */
+        System.out.println("   AMOUNT                                                    ");
+        System.out.println("      |                                                      ");
+        System.out.println(" 3500-|                                                      ");
+        System.out.println("      |                                                      ");
+        System.out.println("      |                                                      ");
+        System.out.println("      |                                 ***                  ");
+        System.out.println("      |                                 ***                  ");
+        System.out.println(" 3000-|                                 ***                  ");
+        System.out.println("      |                                 ***                  ");
+        System.out.println("      |                                 ***                  ");
+        System.out.println("      |                          ***    ***                  ");
+        System.out.println("      |                          ***    ***                  ");
+        System.out.println(" 2500-|     ***                  ***    ***                  ");
+        System.out.println("      |     ***                  ***    ***                  ");
+        System.out.println("      |     ***                  ***    ***                  ");
+        System.out.println("      |     ***                  ***    ***                  ");
+        System.out.println("      |     ***                  ***    ***                  ");
+        System.out.println(" 2000-|     ***           ***    ***    ***                  ");
+        System.out.println("      |     ***           ***    ***    ***                  ");
+        System.out.println("      |     ***           ***    ***    ***                  ");
+        System.out.println("      |     ***           ***    ***    ***                  ");
+        System.out.println("      |     ***    ***    ***    ***    ***                  ");
+        System.out.println(" 1500-|     ***    ***    ***    ***    ***                  ");
+        System.out.println("      |     ***    ***    ***    ***    ***                  ");
+        System.out.println("      |     ***    ***    ***    ***    ***                  ");
+        System.out.println("      |     ***    ***    ***    ***    ***                  ");
+        System.out.println("      |     ***    ***    ***    ***    ***                  ");
+        System.out.println(" 1000-|     ***    ***    ***    ***    ***                  ");
+        System.out.println("      |     ***    ***    ***    ***    ***                  ");
+        System.out.println("      |     ***    ***    ***    ***    ***    ***           ");
+        System.out.println("      |     ***    ***    ***    ***    ***    ***           ");
+        System.out.println("      |     ***    ***    ***    ***    ***    ***           ");
+        System.out.println("  500-|     ***    ***    ***    ***    ***    ***           ");
+        System.out.println("      |     ***    ***    ***    ***    ***    ***           ");
+        System.out.println("      |     ***    ***    ***    ***    ***    ***           ");
+        System.out.println("      |     ***    ***    ***    ***    ***    ***           ");
+        System.out.println("      |     ***    ***    ***    ***    ***    ***           ");
+        System.out.println("    0-|-----***----***----***----***----***----***-----|MONTH");
+        System.out.println("      |     JAN    FEB    MAR    APR    MAY    JUN           ");
+        System.out.println("      |");
+
+        
+        // You can also represent the data in a line chart/pie chart.
+         
+    }
+
+}
