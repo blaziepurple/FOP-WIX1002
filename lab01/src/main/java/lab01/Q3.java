464ff157d567e8a6a3dec18ac6eb2e4f2bf69dcc
Aimaan651 <seren1tye651@gmail.com>
2025-02-26 22:12:45 +0800

    Lab 1

---
+++ lab01/src/main/java/lab01/Q3.java
@@ -0,0 +1,22 @@
+package lab01;
+
+public class Q3 {
+    public static void main(String[] args) {
+        /*
+         * This program's function is to print out basic shapes onto the terminal
+         * using Java
+         */
+        
+        System.out.println("\n");
+        System.out.println("*********        ***            *            *      ");
+        System.out.println("*       *      *     *         ***          * *     ");
+        System.out.println("*       *     *       *       *****        *   *    ");
+        System.out.println("*       *     *       *         *         *     *   ");
+        System.out.println("*       *     *       *         *        *       *  ");
+        System.out.println("*       *     *       *         *         *     *   ");
+        System.out.println("*       *     *       *         *          *   *    ");
+        System.out.println("*       *      *     *          *           * *     ");
+        System.out.println("*********        ***            *            *      ");
+        System.out.println("\n");
+    }
+}
